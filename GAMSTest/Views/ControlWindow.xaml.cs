335ee75f07ec81e3a18152751721741b1026098e
박현철 <tech4@constantec.com>
2026-09-12 22:08:25 +0900

    feat: GAMSTest 전광판 테스트 도구 구현 및 창 종료 생명주기 연동

--- GAMSTest/Views/ControlWindow.xaml.cs
+++ GAMSTest/Views/ControlWindow.xaml.cs
@@ -6,6 +6,7 @@ namespace GAMSTest.Views;
 public partial class ControlWindow : Window
 {
     private readonly DisplayWindow _display;
+    private bool _isClosing;
     public ControlWindow()
     {
         InitializeComponent(); Loaded += (_, _) => { Left = 10; Top = SystemParameters.WorkArea.Bottom - ActualHeight - 10; };
@@ -13,4 +14,22 @@ public partial class ControlWindow : Window
         _display = new DisplayWindow(user); DataContext = new ControlWindowViewModel(_display.Controller.ShowNumbers, _display.Controller.StartPageCycle, _display.Controller.ShowState, _display.Controller.Fill); _display.Show(); _display.Controller.ShowNumbers();
     }
     private void OpenDisplay(object sender, RoutedEventArgs e) { if (!_display.IsVisible) _display.Show(); _display.Activate(); }
+    protected override void OnClosed(EventArgs e)
+    {
+        if (_isClosing)
+        {
+            base.OnClosed(e);
+            return;
+        }
+
+        _isClosing = true;
+        _display.Controller.ShowNumbers();
+        if (_display.IsVisible)
+        {
+            _display.Close();
+        }
+
+        Application.Current.Shutdown();
+        base.OnClosed(e);
+    }
 }
